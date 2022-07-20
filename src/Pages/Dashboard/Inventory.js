635e00a2e7217744099eecec74be8dc47a94ba87
AnikKDev <anikdev2016@gmail.com>
2022-07-20 18:14:03 +0600

    completeed search by category

--- src/Pages/Dashboard/Inventory.js
+++ src/Pages/Dashboard/Inventory.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 import useCategory from '../../hooks/useCategory';
 import useProducts from '../../hooks/useProducts';
+import {
+    useQuery
+} from '@tanstack/react-query';
 import LoadingSpinner from '../SharedPages/LoadingSpinner';
 const Inventory = () => {
-    // get all the products
-    const [products, isLoading, refetch] = useProducts();
 
     // get all the categories
     const [categories] = useCategory();
-    refetch();
-    if (isLoading) {
-        return <LoadingSpinner />
+    const [category, setCategory] = useState('');
+    const [products, setProducts] = useState([]);
+    const [loaded, setLoaded] = useState(false)
+    useEffect(() => {
+        setLoaded(true)
+        fetch(`http://localhost:5000/products/${category}`)
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data);
+                setLoaded(false)
+            })
+    }, [category])
+    const handleSelectedCategory = (e) => {
+        setCategory(e.target.value);
     }
+
     return (
         <div className="my-3 mx-16">
+
             <h1 className="text-4xl text-center">Manage Inventory</h1>
+
 
             <div className="flex justify-between">
                 <Link to='/add-product'><button class="btn">Add Product</button></Link>
 
-
-                <select class="select select-bordered w-full max-w-xs">
-                    <option selected>All Products</option>
+                {/* <button className="btn btn-sm" >All Products</button> */}
+                <select onChange={handleSelectedCategory} class="select select-bordered w-full max-w-xs">
+                    <option>all products</option>
                     {
                         categories?.map(category =>
                             <option>{category.category}</option>
@@ -34,6 +49,9 @@ const Inventory = () => {
             </div>
 
             <div class="overflow-x-auto my-10">
+                {
+                    loaded && <LoadingSpinner />
+                }
                 <table class="table w-full">
                     <thead>
                         <tr>
